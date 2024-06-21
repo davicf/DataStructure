aad9568b93585b8aaa2cab8040efa89b1007b7a5
ADLOJASREDE\davi.freitas <davi.freitas@rarolabs.com.br>
2024-06-21 17:10:39 -0300

    add: binary search tree

---
+++ EstruturaDeDados/BinarySearchTree.cs
@@ -0,0 +1,134 @@
+﻿namespace EstruturaDeDados
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        private NodeTree<T> _root;
+
+        public BinarySearchTree()
+        {
+            _root = null;
+        }
+
+        public void Insert(T item)
+        {
+            if (item is null)
+                return;
+
+            _root = Insert(_root, item);
+        }
+
+        public void Remove(T item)
+        {
+
+        }
+
+        public void DeleteTree(NodeTree<T> currentNode)
+        {
+
+        }
+
+        public NodeTree<T> GetRoot()
+        {
+            return _root;
+        }
+
+        public bool TryGet(T item, out T? itemResult)
+        {
+            if (item is null)
+            {
+                itemResult = default;
+                return false;
+            }
+
+            itemResult = TryGet(item, _root);
+
+            return itemResult is not null;
+        }
+
+        private NodeTree<T> Insert(NodeTree<T> node, T item)
+        {
+            if (node is null)
+                return new NodeTree<T> { Data = item };
+
+            var compareResult = node.Data.CompareTo(item);
+
+            if (compareResult > 0)
+            {
+                node.LeftChild = Insert(node.LeftChild, item);
+            }
+            else if (compareResult < 0)
+            {
+                node.RightChild = Insert(node.RightChild, item);
+            }
+
+            return node;
+        }
+
+        private T? TryGet(T item, NodeTree<T> nodeCurrent)
+        {
+            if (nodeCurrent is null)
+            {
+                return default;
+            }
+
+            var resultCompare = nodeCurrent.Data.CompareTo(item);
+
+            if (resultCompare > 0)
+            {
+                return TryGet(item, nodeCurrent.LeftChild);
+            }
+            else if (resultCompare < 0)
+            {
+                return TryGet(item, nodeCurrent.RightChild);
+            }
+
+            return nodeCurrent.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return _root is null;
+        }
+
+        void PrintPreOrder(NodeTree<T> node)
+        {
+
+        }
+
+        void PrintInOrder(NodeTree<T> node)
+        {
+
+        }
+
+        void PrintPostOrder(NodeTree<T> node)
+        {
+
+        }
+    }
+
+    public class NodeTree<T>
+    {
+        public T Data;
+        public NodeTree<T> LeftChild { get; set; }
+        public NodeTree<T> RightChild { get; set; }
+    }
+
+    public class Student : IComparable<Student>
+    {
+        private static int nextId = 1;
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Student(string name)
+        {
+            Id = nextId++;
+            Name = name;
+        }
+
+        public int CompareTo(Student other)
+        {
+            return Id.CompareTo(other.Id);
+        }
+    }
+}
--- EstruturaDeDados/Program.cs
+++ EstruturaDeDados/Program.cs
@@ -1,60 +1,92 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EstruturaDeDados;
 
-var fila = new EstruturaDeDados.Queue<string>(3);
+ExecuteQueue();
+ExecuteStack();
+ExecuteDynamicQueue();
+ExecuteDynamicStack();
+ExecuteBinarySearchTree();
 
-fila.Enqueue("Primeiro");
-fila.Enqueue("Segundo");
-fila.Enqueue("Terceiro");
-
-Console.WriteLine("Exibindo elementos da fila com " + fila.Count() + " elementos");
-while (!fila.IsEmpty())
+static void ExecuteQueue()
 {
-    Console.WriteLine("Elemento removido: " + fila.Dequeue());
+    var fila = new EstruturaDeDados.Queue<string>(3);
+
+    fila.Enqueue("Primeiro");
+    fila.Enqueue("Segundo");
+    fila.Enqueue("Terceiro");
+
+    Console.WriteLine("Exibindo elementos da fila com " + fila.Count() + " elementos");
+    while (!fila.IsEmpty())
+    {
+        Console.WriteLine("Elemento removido: " + fila.Dequeue());
+    }
 }
 
-var pilha = new EstruturaDeDados.Stack<int>(3);
-
-// Adicionando elementos à pilha
-pilha.Push(1);
-pilha.Push(2);
-pilha.Push(3);
-//pilha.Push(4); // Disparada exceção
-
-Console.WriteLine("Exibindo elementos da pilha com " + pilha.Count() + " elementos");
-while (!pilha.IsEmpty())
+static void ExecuteStack()
 {
-    Console.WriteLine("Elemento removido: " + pilha.Pop());
+    var pilha = new EstruturaDeDados.Stack<int>(3);
+
+    // Adicionando elementos à pilha
+    pilha.Push(1);
+    pilha.Push(2);
+    pilha.Push(3);
+    //pilha.Push(4); // Disparada exceção
+
+    Console.WriteLine("Exibindo elementos da pilha com " + pilha.Count() + " elementos");
+    while (!pilha.IsEmpty())
+    {
+        Console.WriteLine("Elemento removido: " + pilha.Pop());
+    }
 }
 
-DynamicStack<int> dynamicStack = new DynamicStack<int>();
-
-// Adicionando elementos à pilha
-dynamicStack.Push(20);
-dynamicStack.Push(41);
-dynamicStack.Push(5);
-dynamicStack.Push(98);
-dynamicStack.Push(17);
-dynamicStack.Push(22);
-
-Console.WriteLine("Exibindo elementos da pilha dinâmica com " + dynamicStack.Count() + " elementos");
-while (!dynamicStack.IsEmpty())
+static void ExecuteDynamicStack()
 {
-    Console.WriteLine("Elemento removido: " + dynamicStack.Pop());
+    DynamicStack<int> dynamicStack = new DynamicStack<int>();
+
+    dynamicStack.Push(20);
+    dynamicStack.Push(41);
+    dynamicStack.Push(5);
+    dynamicStack.Push(98);
+    dynamicStack.Push(17);
+    dynamicStack.Push(22);
+
+    Console.WriteLine("Exibindo elementos da pilha dinâmica com " + dynamicStack.Count() + " elementos");
+    while (!dynamicStack.IsEmpty())
+    {
+        Console.WriteLine("Elemento removido: " + dynamicStack.Pop());
+    }
 }
 
-DynamicQueue<int> dynamicQueue = new DynamicQueue<int>();
-
-// Adicionando elementos à pilha
-dynamicQueue.Enqueue(20);
-dynamicQueue.Enqueue(41);
-dynamicQueue.Enqueue(5);
-dynamicQueue.Enqueue(98);
-dynamicQueue.Enqueue(17);
-dynamicQueue.Enqueue(22);
-
-Console.WriteLine("Exibindo elementos da fila dinâmica com " + dynamicQueue.Count() + " elementos");
-while (!dynamicQueue.IsEmpty())
+static void ExecuteDynamicQueue()
 {
-    Console.WriteLine("Elemento removido: " + dynamicQueue.Dequeue());
+    DynamicQueue<int> dynamicQueue = new DynamicQueue<int>();
+
+    dynamicQueue.Enqueue(20);
+    dynamicQueue.Enqueue(41);
+    dynamicQueue.Enqueue(5);
+    dynamicQueue.Enqueue(98);
+    dynamicQueue.Enqueue(17);
+    dynamicQueue.Enqueue(22);
+
+    Console.WriteLine("Exibindo elementos da fila dinâmica com " + dynamicQueue.Count() + " elementos");
+    while (!dynamicQueue.IsEmpty())
+    {
+        Console.WriteLine("Elemento removido: " + dynamicQueue.Dequeue());
+    }
+}
+
+static void ExecuteBinarySearchTree()
+{
+    var binaryTree = new BinarySearchTree<Student>();
+
+    binaryTree.Insert(new Student("Name 1"));
+    binaryTree.Insert(new Student("Name 2"));
+    binaryTree.Insert(new Student("Name 3"));
+    var fourthStudent = new Student("Name 4");
+    var fifthStudent = new Student("Name 5");
+    binaryTree.Insert(fifthStudent);
+    binaryTree.Insert(fourthStudent);
+
+    Student? studentResult;
+    var success = binaryTree.TryGet(fifthStudent, out studentResult);
 }
